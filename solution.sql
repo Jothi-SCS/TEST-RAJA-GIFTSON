a278be3c434da9220948e33ff04ff384fba0348e
Jothi-SCS <54353240+Jothi-SCS@users.noreply.github.com>
2026-07-30 14:35:39 +0530

    Create solution.sql

---
+++ solution.sql
@@ -0,0 +1,25 @@
+-- ===========================================
+-- Assignment: Create CollegeDB and Department Table
+-- ===========================================
+
+-- Step 1: Create the database
+-- Write your SQL statement below
+
+
+-- Step 2: Select the database
+-- Write your SQL statement below
+
+
+-- Step 3: Create the Department table
+/*
+Table Name: Department
+
+Fields:
+------------------------------------------
+DepartmentID    Number(5)   Primary Key
+DepartmentName  Varchar(20)
+HOD             Varchar(20)
+------------------------------------------
+*/
+
+-- Write your CREATE TABLE statement below
